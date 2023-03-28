b44a6130768164f84598e66943bfaecadd625dc8
mkaesz <mskaesz@msk.pub>
2023-03-28 21:48:20 +0200

    added complex vars

--- variables.tf
+++ variables.tf
@@ -11,6 +11,17 @@ variable "db_username" {
 variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
-  default = "Blub"
 }
 
+variable "storage_config" {
+  type = list(object({
+    name                      = string
+    account_kind              = string
+    account_tier              = string
+    account_replication_type  = string
+    access_tier               = string
+    enable_https_traffic_only = bool
+    min_tls_version           = string
+    is_hns_enabled            = bool
+  }))
+}
